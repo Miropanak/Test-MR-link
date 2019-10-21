a2802f9de78326653ac065cfdbf1814a05fa73ad
bencatg <bencatg@intechon.sk>
2019-10-21 21:45:20 +0200

    swagger documentation implemented

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="http://team09-19.studenti.fiit.stuba.sk/",
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Team project generated Api documentation",
+ *         description="...",
+ *         termsOfService="TOS",
+ *         @SWG\Contact(
+ *             email="fiit.tim09@gmail.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Private License",
+ *             url="/"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Team webpage",
+ *         url="http://labss2.fiit.stuba.sk/TeamProject/2019/team09/"
+ *     )
+ * )
+ */
+class SwaggerController
+{
+
+}
