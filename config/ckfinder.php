31888f3b86f1d67a7ca9558790ffb495f9b71a43
bencatg <bencatg@intechon.sk>
2019-12-02 13:45:13 +0100

    Change config

--- config/ckfinder.php
+++ config/ckfinder.php
@@ -22,7 +22,7 @@
 
 $config = array();
 
-$config['authentication'] = '\App\Http\Middleware\CustomCKFinderAuth';
+$config['authentication'] = function () { return false; };
 /*============================ License Key ============================================*/
 // http://docs.cksource.com/ckfinder3-php/configuration.html#configuration_options_licenseKey
 
