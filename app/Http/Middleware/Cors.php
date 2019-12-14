65d53724471b1786da6695f6e15f3efaabd35120
Gregor Benčať <bencat.gregor@gmail.com>
2019-12-14 21:09:44 +0000

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,7 +6,6 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        Log::debug($request);
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
