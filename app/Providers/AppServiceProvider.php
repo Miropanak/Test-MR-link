5f9db5c6c9841b15055ea985a113558330b7e9e2
bencat <bencat.gregor@gmail.com>
2020-02-17 13:25:49 +0100

    add dont discover dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment('production')) {
-            return;
-        }
+        //
     }
 
     /**
