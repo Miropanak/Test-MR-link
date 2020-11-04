228fed40f900b7ed03ab79b4febf731fa5cac0cb
Adam Čierny <ciernya@gmail.com>
2020-11-04 16:08:34 +0000

    Revert "Merge branch 'Roli-modify-create-events-api' into 'develop'"
    This reverts merge request !99

--- app/Models/EventCategory.php
+++ app/Models/EventCategory.php
@@ -8,13 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class EventCategory extends Model
 {
     use HasFactory;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'event_id','category_id',
-    ];
-
 }
